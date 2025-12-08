8b8335d190a4eb11bca277f59435528ce7a24995
FrehiwetZ <papsye30@gmail.com>
2025-12-08 01:09:11 -0900

    givind access to the message class to send massage to all connected clients

--- src/main/java/com/chat/app/config/WebSockerConfig.java
+++ src/main/java/com/chat/app/config/WebSockerConfig.java
@@ -1,6 +1,7 @@
 package com.chat.app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,6 +14,15 @@ public class WebSockerConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndPoint("/chat")
                 .setAllowedOrigins("http://localhost:5173")
                 .withSockjs();
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //set message broker
+        registry.enableSimpleBroker("/topic");
+        //expect message with /app/sendmessage
+        registry.setApplicationDestinationPrefixes("/app");
 
     }
 }
