b36d057e4162b359ee2e726739a6bf4912349d85
FrehiwetZ <papsye30@gmail.com>
2025-12-08 00:55:02 -0900

    making the message more accessable

---
+++ src/main/java/com/chat/app/config/WebSockerConfig.java
@@ -0,0 +1,18 @@
+package com.chat.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSockerConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndPoint("/chat")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockjs();
+
+    }
+}
